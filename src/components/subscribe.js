7a8de1ad76bac5cf348f6b6435e45f577780dcf7
Rich Cook <flaturtha@users.noreply.github.com>
2020-08-18 13:01:16 -0500

    Update subscribe.js
    Spelling error

--- src/components/subscribe.js
+++ src/components/subscribe.js
@@ -5,7 +5,7 @@ const Books = props => (
   <section className="container mx-auto mt-0 lg:py-12 lg:px-20" id="free-story">
     <div className="mt-2 p-8 md:w-4/6 mx-auto">
     
-      <h1>SUBSCRIBE FOR HERE!</h1>
+      <h1>SUBSCRIBE FORM HERE!</h1>
 
     </div>
 
