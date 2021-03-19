a436256ae8c585119bc5649f95264b5582686ebe
flaturtha <rich@talesofmurder.com>
2021-03-19 10:34:10 -0500

    adds fb pixel plugin & pixel id

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,35 +13,11 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-facebook-pixel`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-176001701-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        // siteSpeedSampleRate: 10,
-        // sampleRate: 5,
-        // cookieDomain: "example.com",
+        pixelId: "2702516106515170",
       },
     },
-
     {
       resolve: 'gatsby-plugin-anchor-links',
       options: {
